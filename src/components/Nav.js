cf5d837e1c61c59738c7577aec84803a4aa0af94
tansiewling-hotmail <prudence_tan@hotmail.com>
2020-01-03 17:46:06 +0800

    Fixed: Cannot collapse menu item when screen is small

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,24 +5,18 @@ class Nav extends React.Component {
         return(
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light sticky-top">
-                    <a className="navbar-brand col-6" href="#">
-                        <img className="col col-2" src="img/logo.png" alt="Tan Siew Ling - Up Your Software Quality" title="Tan Siew Ling - Up Your Software Quality" />
+                    <a className="navbar-brand" href="/">
+                        <img src="img/logo.png" width="100" height="100" className="d-inline-block align-center mr-5" alt="Tan Siew Ling - Up Your Software Quality" title="Tan Siew Ling - Up Your Software Quality" />
                     </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#portfolio">Portfolio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact">Contact</a>
-                            </li>
-                        </ul>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link" href="#about">About</a>
+                            <a className="nav-item nav-link" href="#portfolio">Portfolio</a>
+                            <a className="nav-item nav-link" href="#contact">Contact</a>
+                        </div>
                     </div>
                 </nav>
             </div>
