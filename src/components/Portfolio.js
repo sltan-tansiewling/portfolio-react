30e9ac65e5189c8339776bdb5c42073fcb205342
tansiewling-hotmail <prudence_tan@hotmail.com>
2020-01-07 13:51:37 +0800

    Add demo and view code links for projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,6 +25,14 @@ class Portfolio extends React.Component {
                                             <h5 className="card-title">Save-the-Earth</h5>
                                             <p className="card-text">An educational game to educate the public on the concepts of recycling while having fun.</p>
                                             <p className="card-text"><small className="text-muted">Technologies Used: HTML, CSS, JavaScript</small></p>
+
+                                            <a href="https://tansiewling-hotmail.github.io/save-the-earth/" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio mr-4">Demo</button>
+                                            </a>
+
+                                            <a href="https://github.com/tansiewling-hotmail/save-the-earth" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio">View Code</button>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -40,6 +48,13 @@ class Portfolio extends React.Component {
                                             <h5 className="card-title">HR System</h5>
                                             <p className="card-text">A HR system to allow employees to apply and keep track of their leave applications.</p>
                                             <p className="card-text"><small className="text-muted">Technologies Used: Node JS, Express JS, PostgreSQL</small></p>
+                                            <a href="https://human-resource-system.herokuapp.com/" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio mr-4">Demo</button>
+                                            </a>
+
+                                            <a href="https://github.com/tansiewling-hotmail/hr_system" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio">View Code</button>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -55,6 +70,14 @@ class Portfolio extends React.Component {
                                             <h5 className="card-title">Minimum Quantity</h5>
                                             <p className="card-text">A social network platform to allow users to consolidate their food orders to get group discount when minimum quantity or spending specified by the restaurant is met.</p>
                                             <p className="card-text"><small className="text-muted">Technologies Used: Ruby on Rails, PostgreSQL</small></p>
+
+                                            <a href="https://github.com/tansiewling-hotmail/mq" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio mr-4 disabled">Demo</button>
+                                            </a>
+
+                                            <a href="https://github.com/tansiewling-hotmail/mq" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio">View Code</button>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -70,6 +93,14 @@ class Portfolio extends React.Component {
                                             <h5 className="card-title">Pocket Membership</h5>
                                             <p className="card-text">A web application to hold all your membership accounts on one platform.</p>
                                             <p className="card-text"><small className="text-muted">Technologies Used: Ruby on Rails, PostgreSQL</small></p>
+
+                                            <a href="https://pocket-membership.herokuapp.com/" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio mr-4">Demo</button>
+                                            </a>
+
+                                            <a href="https://github.com/tansiewling-hotmail/pocket-membership" target="_blank">
+                                                <button type="button" className="btn btn-outline-dark btnViewPortfolio">View Code</button>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
