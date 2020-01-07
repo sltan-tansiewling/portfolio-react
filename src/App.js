bd364069c267d5cabae486b301f754ef71b1ec60
tansiewling-hotmail <prudence_tan@hotmail.com>
2020-01-07 16:02:22 +0800

    No code change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 class App extends React.Component {
+
     render() {
+
         return (
             <div className="App">
                 <Nav />
